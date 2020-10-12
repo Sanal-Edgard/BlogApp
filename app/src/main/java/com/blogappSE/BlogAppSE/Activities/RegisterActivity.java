b4e3d9919ed67966654ad6fda995b5e737a552aa
edgard <ednokia8@gmail.com>
2020-10-12 11:21:10 +0300

    finished second lesson

--- app/src/main/java/com/blogappSE/BlogAppSE/Activities/RegisterActivity.java
+++ app/src/main/java/com/blogappSE/BlogAppSE/Activities/RegisterActivity.java
@@ -1,27 +1,80 @@
 package com.blogappSE.BlogAppSE.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.blogappSE.BlogAppSE.R;
 
 public class RegisterActivity extends AppCompatActivity {
-    ImageView regUserPhoto;
+    ImageView ImgUserPhoto;
+    public static int PReqCode = 1;
+    public static int REQUESCODE = 1;
+    Uri pickedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        regUserPhoto = findViewById(R.id.regUserPhoto);
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
 
-        regUserPhoto.setOnClickListener(new View.OnClickListener() {
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(Build.VERSION.SDK_INT >= 22) {
+                    CheckAndRequestForPermisson();
+                }
+                else {
+                    openGalery();
+                }
             }
         });
+    }
+
+    private void CheckAndRequestForPermisson() {
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "Please accept for required permission", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(RegisterActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+        }
+        else {
+            openGalery();
+        }
+    }
+
+
+    private void openGalery() {
+        //Открыть галерею и ждать, пока пользователь выберет фото
+        Intent galeryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galeryIntent.setType("image/*");
+        startActivityForResult(galeryIntent, REQUESCODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode ==  RESULT_OK && requestCode == REQUESCODE && data != null) {
+            //юзер успешно выбрал фото
+            //нам нужно сохранить его выбор в uri
+            pickedImageUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImageUri);
+        }
     }
 }
