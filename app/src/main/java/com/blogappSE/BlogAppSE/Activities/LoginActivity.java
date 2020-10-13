99987b605c9fd3f76ee986edb10b5c404215900f
edgard <ednokia8@gmail.com>
2020-10-13 11:15:05 +0300

    4th lesson completed

---
+++ app/src/main/java/com/blogappSE/BlogAppSE/Activities/LoginActivity.java
@@ -0,0 +1,111 @@
+package com.blogappSE.BlogAppSE.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.blogappSE.BlogAppSE.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userMail, userPassword;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent HomeActivity;
+    private ImageView loginPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        userMail = findViewById(R.id.login_mail);
+        userPassword = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.loginBtn);
+        loginProgress = findViewById(R.id.login_progress);
+        mAuth = FirebaseAuth.getInstance();
+        loginPhoto = findViewById(R.id.login_photo);
+        HomeActivity = new Intent(this, HomeActivity.class);
+
+        loginPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+            }
+        });
+
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if(mail.isEmpty() || password.isEmpty()) {
+                    showMessage("Please, verify all fields");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    signIn(mail, password);
+                }
+            }
+        });
+    }
+
+    private void signIn(String mail, String password) {
+        mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+                else {
+                    showMessage(task.getException().getMessage());
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null) {
+            //пользователь уже залогинился и нам его нужно перенаправить на домашнюю страницу.
+            updateUI();
+        }
+    }
+}
