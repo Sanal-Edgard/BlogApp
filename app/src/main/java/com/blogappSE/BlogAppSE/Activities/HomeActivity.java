b3b365a237651f0857da136ff07d151c8d318e38
edgard <ednokia8@gmail.com>
2020-10-13 10:02:42 +0300

    we've done the 3th lesson and was able to realize a sign-up method.

---
+++ app/src/main/java/com/blogappSE/BlogAppSE/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.blogappSE.BlogAppSE.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blogappSE.BlogAppSE.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/blogappSE/BlogAppSE/Activities/RegisterActivity.java
+++ app/src/main/java/com/blogappSE/BlogAppSE/Activities/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.blogappSE.BlogAppSE.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,22 +13,70 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.blogappSE.BlogAppSE.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
     ImageView ImgUserPhoto;
     public static int PReqCode = 1;
     public static int REQUESCODE = 1;
     Uri pickedImageUri;
+    private EditText userEmail, userPassword, userPassword2, userName;
+    private ProgressBar loadingProgress;
+    private FirebaseAuth mAuth;
+    private Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ImgUserPhoto = findViewById(R.id.regUserPhoto);
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPassword);
+        userPassword2 = findViewById(R.id.regPassword2);
+        userName = findViewById(R.id.regName);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+        loadingProgress.setVisibility(View.INVISIBLE);
+        mAuth = FirebaseAuth.getInstance();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+                final String name = userName.getText().toString();
+
+                if(email.isEmpty() || name.isEmpty() || password.isEmpty() || password2.isEmpty() || !password.equals(password2)) {
+                    showMessage("Please Verify all field");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    //если все ок и все поля заполнены то мы можем создать наш аккаунт
+                    //CreateUserAccount - этот метод  попытается создать пользователя, если его емайл валидный.
+                    CreateUserAccount(email, name, password);
+                }
+            }
+        });
 
         ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +89,78 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void CreateUserAccount(String email, final String name, String password) {
+        //этот метод создает пользователя со спецефическими емайл и пароль
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            //аккаунт успешно создан
+                            showMessage("acount created");
+                            //после создания акаунта нам нужно обновить эго фото профиля и имя.
+                            updateUserInfo(name, pickedImageUri, mAuth.getCurrentUser());
+                        }
+                        else {
+                            //ошибка создания аккаунта
+                            showMessage("account creation failed " + task.getException().getMessage());
+                            regBtn.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+    }
+
+    //обновляем фото профиля и имя пользователя.
+    private void updateUserInfo(final String name, Uri pickedImageUri, final FirebaseUser currentUser) {
+        //во первых нам нужно обновить фото профиля в firebasestorage и получить его uri
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = mStorage.child(pickedImageUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //фото загружено успешно
+                //сейчас мы можем получить uri зображения
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        //uri одержит uri фото профиля
+
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()) {
+                                            //нформация о пользователе обнавлена успешна
+                                            showMessage("Register Complete");
+                                            updateUri();
+                                        }
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+
+
+
+    }
+
+    private void updateUri() {
+        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     private void CheckAndRequestForPermisson() {
