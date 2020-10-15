84146f74f13447795a95155e16ac76c1aa380221
edgard <ednokia8@gmail.com>
2020-10-15 09:39:01 +0300

    6th lesson was done

--- app/src/main/java/com/blogappSE/BlogAppSE/Activities/Home.java
+++ app/src/main/java/com/blogappSE/BlogAppSE/Activities/Home.java
@@ -1,12 +1,16 @@
 package com.blogappSE.BlogAppSE.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.TextureView;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.blogappSE.BlogAppSE.Fragments.HomeFragment;
+import com.blogappSE.BlogAppSE.Fragments.ProfileFragment;
+import com.blogappSE.BlogAppSE.Fragments.SettingsFragment;
 import com.blogappSE.BlogAppSE.R;
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,7 +19,11 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -39,27 +47,56 @@ public class Home extends AppCompatActivity {
         //init
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
-
         setSupportActionBar(toolbar);
+
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//
+//
+//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_profile, R.id.nav_settings, R.id.nav_sign_out)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
         updateNavHeader();
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                int id = destination.getId();
+
+                if(id == R.id.nav_home) {
+                    getSupportActionBar().setTitle("Home");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new HomeFragment()).commit();
+                }
+                else if(id == R.id.nav_profile) {
+                    getSupportActionBar().setTitle("Profile");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new ProfileFragment()).commit();
+                }
+                else if(id == R.id.nav_settings) {
+                    getSupportActionBar().setTitle("Settings");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new SettingsFragment()).commit();
+                }
+                else if(id == R.id.nav_sign_out) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent loginActivity = new Intent(Home.this, LoginActivity.class);
+                    startActivity(loginActivity);
+                    finish();
+                }
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        });
     }
 
     @Override
